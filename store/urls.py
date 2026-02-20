36fca052c735ad161747ba65436f7e127e54eb95
sudev <sudevks2006@gmail.com>
2026-02-20 22:24:09 +0530

    Restructured templates folder and updated views

--- store/urls.py
+++ store/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from . import views
+from store import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('products/', views.product_list, name='product_list'),
-    path('product/', views.product_detail, name='product_detail'),
-    path('cart/', views.cart, name='cart'),
-    path('checkout/', views.checkout, name='checkout'),
+    path('', views.index, name='index'),
+    #path('products/', views.product_list, name='product_list'),
+    #path('product/', views.product_detail, name='product_detail'),
+    #path('cart/', views.cart, name='cart'),
+   # path('checkout/', views.checkout, name='checkout'),
 ]
--- store/views.py
+++ store/views.py
@@ -1,16 +1,15 @@
 from django.shortcuts import render
 
-def home(request):
-    return render(request, 'home.html')
+def index(request):
+    return render(request, 'core/index.html')
+#def product_list(request):
+   # return render(request, 'product_list.html')
 
-def product_list(request):
-    return render(request, 'product_list.html')
+#def product_detail(request):
+    #return render(request, 'product_detail.html')
 
-def product_detail(request):
-    return render(request, 'product_detail.html')
+#def cart(request):
+    #return render(request, 'cart.html')
 
-def cart(request):
-    return render(request, 'cart.html')
-
-def checkout(request):
-    return render(request, 'checkout.html')
+#def checkout(request):
+    #return render(request, 'checkout.html')
